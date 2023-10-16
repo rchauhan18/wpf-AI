8515a6cc630b5338ed2af060d967a0d88834d051
Rishabh Chauhan <rchauhan@microsoft.com>
2023-10-16 17:33:51 +0530

    Replacing BF

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/dataobject.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/dataobject.cs
@@ -12,6 +12,7 @@
 //
 
 
+
 namespace System.Windows
 {
     using System;
@@ -3146,8 +3147,19 @@ namespace System.Windows
                     // {
                     //     value = null;
                     // }
-                    BinaryReader Br = new BinaryReader(stream, Encoding.UTF8, leaveOpen: true);
-                    value = Br.ReadString();
+
+                    long startPosition = stream.Position;
+                    try
+                    {
+                        if (new BinaryFormattedObject(stream, leaveOpen: true).TryGetFrameworkObject(out object val))
+                        {
+                            return val;
+                        }
+                    }
+                    catch  
+                    {
+                        // Couldn't parse for some reason, let the BinaryFormatter try to handle it.
+                    }
                 }
                 else
                 {
